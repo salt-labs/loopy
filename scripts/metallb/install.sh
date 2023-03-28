82162ebb0745a134ce8b4c9146611ca55375842c
MAHDTech <MAHDTech@saltlabs.tech>
2023-03-29 01:15:55 +1100

    feat: more manifests

--- scripts/metallb/install.sh
+++ scripts/metallb/install.sh
@@ -15,7 +15,12 @@ docker network inspect -f '{{.IPAM.Config}}' kind
 kubectl apply -f "${SCRIPT_DIR}/config.yaml"
 
 LB_IP=$(kubectl get svc/test -n test -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
 echo "LoadBalancer IP: ${LB_IP}"
+
 for _ in {1..10}; do
+
+	# curl -H "Host: example.com" http://192.0.2.1/
 	curl --verbose ${LB_IP}:80
+
 done

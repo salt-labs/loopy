9c0d8ad056b34aa5013f3189457f43d502e525ab
MAHDTech <MAHDTech@saltlabs.tech>
2023-04-21 12:53:25 +0000

    feat: WIP add apt-mirror

--- config/manifests/apt-mirror/entrypoint.sh
+++ config/manifests/apt-mirror/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # https://www.wcooke.org/2021/02/debian-mirror-kubernetes/
+# http://au.archive.ubuntu.com/ubuntu/pool/main/l/linux/linux-headers-5.4.0-97-generic_5.4.0-97.110_amd64.deb
 
 set -euo pipefail
 
